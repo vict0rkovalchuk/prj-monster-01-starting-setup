312d5219a21fee082ee8ef91f544477996f1337f
Victor Kovalchuk <vict02r05@gmail.com>
2026-09-15 20:50:19 +0300

    adding a 'game over' screen

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = Vue.createApp({
     return {
       monsterHealth: 100,
       playerHealth: 100,
-      currentRound: 0
+      currentRound: 0,
+      winner: null
     };
   },
   computed: {
@@ -19,6 +20,22 @@ const app = Vue.createApp({
     },
     isFreeRound() {
       return this.currentRound % 3 === 0;
+    }
+  },
+  watch: {
+    monsterHealth(value) {
+      if(value <= 0 && this.playerHealth <= 0) {
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        this.winner = 'player';
+      }
+    },
+    playerHealth(value) {
+      if(value <= 0 && this.monsterHealth <= 0) {
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        this.winner = 'monster';
+      }
     }
   },
   methods: {
